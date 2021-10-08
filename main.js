ec9e9d3cb40702db8a111f05d9f09fcc6b87358f
Karol Skalik <48031760+Celmaibun@users.noreply.github.com>
2021-10-08 18:46:43 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const buttonsWrapper = document.querySelector('.buttons');
 const wordList = document.querySelector('.word-list');
 const winWrapper = document.querySelector('.win');
 const loseWrapper = document.querySelector('.lose');
-const livesDiv = document.querySelector('.fa-heart');
+const livesDiv = document.querySelector('.lives-text');
 
 const alphabet = [
   'a',
@@ -34,6 +34,8 @@ const alphabet = [
 ];
 
 const words = [
+  'foodstuff',
+  'liziaard',
   'pest',
   'fly',
   'threshold',
@@ -51,33 +53,34 @@ const words = [
 const generatedNumber = Math.floor(Math.random() * words.length);
 let wordArray = [];
 let lives = 10;
-let scores = 0;
 
-const displayButtons = function () {
-  alphabet.forEach(function (letter) {
-    const button = document.createElement('button');
-    button.classList.add('button-alphabet');
-    button.textContent = letter.toUpperCase();
-    button.value = letter;
-    buttonsWrapper.append(button);
-  });
-};
-displayButtons();
-//REMOVE BUTTON WITH VALUE THIS FROM LETTER CLICK FUNCTION
+//Remove button when click
 const removeButton = function (value) {
   buttonsWrapper.removeChild(value);
 };
+//Remove all li from ul
 const removeLi = function () {
   while (wordList.firstChild) {
     wordList.removeChild(wordList.firstChild);
   }
 };
+//Push '_' to array
 const pushSymbolToArray = function () {
   for (let i = 0; i < words[generatedNumber].length; i++) {
     wordArray.push('_');
   }
 };
-///////////////
+//More index finder
+function indexOfAll(array, searchItem) {
+  var i = array.indexOf(searchItem),
+    indexes = [];
+  while (i !== -1) {
+    indexes.push(i);
+    i = array.indexOf(searchItem, ++i);
+  }
+  return indexes;
+}
+//Check for win/lose
 const checkWinner = function (word) {
   const array = wordArray.join('');
   const winnerArray = word;
@@ -88,6 +91,7 @@ const checkWinner = function (word) {
     winWrapper.classList.remove('hidden');
   }
 };
+//Display letter, create new li
 const displayLetters = function (word) {
   for (let i = 0; i < word.length; i++) {
     const li = document.createElement('li');
@@ -101,20 +105,26 @@ displayLetters(words[generatedNumber]);
 
 const letterClick = function () {
   const word = words[generatedNumber];
+  //Check if there is key letter in word
   if (word.indexOf(this.value) !== -1) {
-    wordArray.splice(word.indexOf(this.value), 1, this.value);
+    const indexes = indexOfAll(word, this.value);
+    for (let i = 0; i < indexes.length; i++) {
+      wordArray.splice(indexes[i], 1, this.value);
+    }
     removeLi();
-    displayLetters(words[generatedNumber]);
+    displayLetters(word);
     removeButton(this);
-  } else {
+  }
+  //If there is no key letter in word
+  else {
     removeButton(this);
     lives--;
   }
-  console.log(wordArray);
   livesDiv.textContent = ` ${lives} lives`;
   checkWinner(word);
 };
 
 document.querySelectorAll('.button-alphabet').forEach(item => {
   item.addEventListener('click', letterClick);
+  item.addEventListener('.touch', letterClick);
 });
